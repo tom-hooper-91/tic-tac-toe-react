fea83d3f85426f7a655018807a4eb280aac9a8dd
Tom Hooper <tomwbhooper@hotmail.com>
2021-06-19 15:03:37 +1200

    add useState to onClick function

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,34 +8,47 @@ function Square({ value, onClick }) {
     <button className="square" onClick={onClick}>
       {value}
     </button>
-  );
+  )
 }
 
 function Game() {
 
+    const [ squares, setSquares ] = useState(Array(9).fill(null));
+
+    function renderSquare(i) {
+        return <Square
+        value={squares[i]}
+        onClick={() => {
+            const nextSquares = squares.slice()
+            nextSquares[i] = 'X'
+            setSquares(nextSquares)
+        }}
+        />
+    }
+
     return (
-      <div className="container">
+        <div className="container">
         <div className="game">
-          <div className="game-board">
+            <div className="game-board">
             <div className="board-row">
-              <Square value={0} onClick={null}/>
-              <Square value={1} onClick={null}/>
-              <Square value={2} onClick={null}/>
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div className="board-row">
-              <Square value={3} onClick={null}/>
-              <Square value={4} onClick={null}/>
-              <Square value={5} onClick={null}/>
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div className="board-row">
-              <Square value={6} onClick={null}/>
-              <Square value={7} onClick={null}/>
-              <Square value={8} onClick={null}/>
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
-          </div>
+            </div>
         </div>
-      </div>
-    );
+        </div>
+    )
   }
   
   ReactDOM.render(<Game />, document.getElementById("root"));
